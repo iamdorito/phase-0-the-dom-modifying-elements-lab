98cf4713b347f38d1d7013c314a3a0f01fa70bdf
Dorothy <dorothychauu@gmail.com>
2022-11-30 20:27:12 -0500

    commit tree 19df2489f35ee9f17ff53f9d29d1d1f4af46ed21

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Write your code here!\
+const element = document.getElementById("main");
+element.remove(element);
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = ("victory");
+
+newHeader.innerHTML = "DOROTHY is the champion";
